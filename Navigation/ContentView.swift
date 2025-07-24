3498b269e5d8eb9a988c5859ccc1fffa208275d4
Haley Sheth <haley.sheth@gmail.com>
2025-07-24 10:09:01 -0400

    Navigating links practice (step one)

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -10,15 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+Text("This is the home page")
+                .font(.title3)
+                .fontWeight(.thin)
+
         }
-        .padding()
     }
 }
+
+
+
+
 
 #Preview {
     ContentView()
 }
+
